5daaf89b08373ed466a4150428a232af049ef5a8
Raman077 <ramanprabhakar007@gmail.com>
2020-05-23 22:03:01 +0530

    initial commit

---
+++ bluedit/src/main/java/com/project/bluedit/BlueditApplication.java
@@ -0,0 +1,13 @@
+package com.project.bluedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueditApplication.class, args);
+	}
+
+}
---
+++ bluedit/src/test/java/com/project/bluedit/BlueditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bluedit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
