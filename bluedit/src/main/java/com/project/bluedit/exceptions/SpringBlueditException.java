2d673fb7d3b42e79ec223f1da835b7c13d19da4c
Raman077 <ramanprabhakar007@gmail.com>
2020-06-06 20:05:09 +0530

    add mail service

---
+++ bluedit/src/main/java/com/project/bluedit/exceptions/SpringBlueditException.java
@@ -0,0 +1,7 @@
+package com.project.bluedit.exceptions;
+
+public class SpringBlueditException extends RuntimeException {
+    public SpringBlueditException(String message ) {
+        super(message);
+    }
+}
--- bluedit/src/main/java/com/project/bluedit/service/AuthService.java
+++ bluedit/src/main/java/com/project/bluedit/service/AuthService.java
@@ -47,6 +47,4 @@ public class AuthService {
         verificationTokenRepository.save(verificationToken);
         return token;
     }
-
-
 }
